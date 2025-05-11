5f11ba55c9330e8d739822bd3523badde07c4bea
AnujSinghML <anujsingh882003@gmail.com>
2025-05-12 03:49:28 +0530

    images fix

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -13,6 +13,7 @@ export async function OPTIONS() {
   return new NextResponse(null, {
     status: 204,
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
